03957ee05faa55034a52c50e4bf2de904daf7847
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-04-17 10:40:52 -0700

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,7 +36,23 @@ namespace FinalProject_Winter2021
             this.domainUpDownService = new System.Windows.Forms.DomainUpDown();
             this.labelService = new System.Windows.Forms.Label();
             this.buttonReview = new System.Windows.Forms.Button();
-            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.maskedTextBoxPhone = new System.Windows.Forms.MaskedTextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelRequired = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -53,7 +69,7 @@ namespace FinalProject_Winter2021
             // 
             this.dateTimePickerBooking.CustomFormat = "yyyy/MM/dd HH:mm";
             this.dateTimePickerBooking.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePickerBooking.Location = new System.Drawing.Point(178, 84);
+            this.dateTimePickerBooking.Location = new System.Drawing.Point(222, 36);
             this.dateTimePickerBooking.Name = "dateTimePickerBooking";
             this.dateTimePickerBooking.Size = new System.Drawing.Size(200, 31);
             this.dateTimePickerBooking.TabIndex = 4;
@@ -61,7 +77,7 @@ namespace FinalProject_Winter2021
             // labelApoTime
             // 
             this.labelApoTime.AutoSize = true;
-            this.labelApoTime.Location = new System.Drawing.Point(12, 84);
+            this.labelApoTime.Location = new System.Drawing.Point(20, 42);
             this.labelApoTime.Name = "labelApoTime";
             this.labelApoTime.Size = new System.Drawing.Size(160, 25);
             this.labelApoTime.TabIndex = 5;
@@ -69,7 +85,7 @@ namespace FinalProject_Winter2021
             // 
             // domainUpDownService
             // 
-            this.domainUpDownService.Location = new System.Drawing.Point(178, 130);
+            this.domainUpDownService.Location = new System.Drawing.Point(222, 71);
             this.domainUpDownService.Name = "domainUpDownService";
             this.domainUpDownService.Size = new System.Drawing.Size(200, 31);
             this.domainUpDownService.TabIndex = 7;
@@ -78,7 +94,7 @@ namespace FinalProject_Winter2021
             // labelService
             // 
             this.labelService.AutoSize = true;
-            this.labelService.Location = new System.Drawing.Point(12, 132);
+            this.labelService.Location = new System.Drawing.Point(20, 74);
             this.labelService.Name = "labelService";
             this.labelService.Size = new System.Drawing.Size(67, 25);
             this.labelService.TabIndex = 8;
@@ -93,14 +109,171 @@ namespace FinalProject_Winter2021
             this.buttonReview.Text = "REVIEW";
             this.buttonReview.UseVisualStyleBackColor = true;
             // 
-            // buttonCreate
+            // buttonNew
             // 
-            this.buttonCreate.Location = new System.Drawing.Point(437, 44);
-            this.buttonCreate.Name = "buttonCreate";
-            this.buttonCreate.Size = new System.Drawing.Size(112, 34);
-            this.buttonCreate.TabIndex = 10;
-            this.buttonCreate.Text = "BOOK";
-            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonNew.Location = new System.Drawing.Point(437, 44);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(112, 34);
+            this.buttonNew.TabIndex = 10;
+            this.buttonNew.Text = "NEW";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(20, 112);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(97, 25);
+            this.labelFirstName.TabIndex = 11;
+            this.labelFirstName.Text = "First Name";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(20, 149);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(95, 25);
+            this.labelLastName.TabIndex = 12;
+            this.labelLastName.Text = "Last Name";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(222, 108);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(200, 31);
+            this.textBoxFirstName.TabIndex = 13;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(222, 145);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(200, 31);
+            this.textBoxLastName.TabIndex = 14;
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Location = new System.Drawing.Point(20, 186);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(62, 25);
+            this.labelPhone.TabIndex = 16;
+            this.labelPhone.Text = "Phone";
+            // 
+            // maskedTextBoxPhone
+            // 
+            this.maskedTextBoxPhone.Location = new System.Drawing.Point(222, 182);
+            this.maskedTextBoxPhone.Mask = "(999) 000-0000";
+            this.maskedTextBoxPhone.Name = "maskedTextBoxPhone";
+            this.maskedTextBoxPhone.Size = new System.Drawing.Size(200, 31);
+            this.maskedTextBoxPhone.TabIndex = 17;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(20, 223);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(61, 25);
+            this.labelEmail.TabIndex = 18;
+            this.labelEmail.Text = "E-mail";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(222, 219);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(200, 31);
+            this.textBoxEmail.TabIndex = 19;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.domainUpDownService);
+            this.groupBox1.Controls.Add(this.textBoxEmail);
+            this.groupBox1.Controls.Add(this.dateTimePickerBooking);
+            this.groupBox1.Controls.Add(this.labelEmail);
+            this.groupBox1.Controls.Add(this.labelApoTime);
+            this.groupBox1.Controls.Add(this.maskedTextBoxPhone);
+            this.groupBox1.Controls.Add(this.labelService);
+            this.groupBox1.Controls.Add(this.labelPhone);
+            this.groupBox1.Controls.Add(this.labelFirstName);
+            this.groupBox1.Controls.Add(this.textBoxLastName);
+            this.groupBox1.Controls.Add(this.labelLastName);
+            this.groupBox1.Controls.Add(this.textBoxFirstName);
+            this.groupBox1.Controls.Add(this.labelRequired);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(40, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(453, 285);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Appointment Information";
+            // 
+            // labelRequired
+            // 
+            this.labelRequired.AutoSize = true;
+            this.labelRequired.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelRequired.ForeColor = System.Drawing.Color.Red;
+            this.labelRequired.Location = new System.Drawing.Point(174, 38);
+            this.labelRequired.Name = "labelRequired";
+            this.labelRequired.Size = new System.Drawing.Size(24, 29);
+            this.labelRequired.TabIndex = 21;
+            this.labelRequired.Text = "*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(546, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 29);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(76, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 29);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(108, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 29);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(110, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 29);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(81, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 29);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "*";
             // 
             // Form1
             // 
@@ -108,16 +281,16 @@ namespace FinalProject_Winter2021
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1116, 705);
-            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonNew);
             this.Controls.Add(this.buttonReview);
-            this.Controls.Add(this.labelService);
-            this.Controls.Add(this.domainUpDownService);
-            this.Controls.Add(this.labelApoTime);
-            this.Controls.Add(this.dateTimePickerBooking);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Appointment Management System";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,7 +303,22 @@ namespace FinalProject_Winter2021
         private System.Windows.Forms.DomainUpDown domainUpDownService;
         private System.Windows.Forms.Label labelService;
         private System.Windows.Forms.Button buttonReview;
-        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPhone;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelRequired;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
